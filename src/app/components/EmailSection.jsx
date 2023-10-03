53dfd5aa3e85da0017bfe8bcc40d9894fecbc9df
HenryLooo <94285262+HenryLooo@users.noreply.github.com>
2023-10-04 00:38:09 +0700

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,32 +8,7 @@ import Link from "next/link"
 import Image from "next/image"
 
 const EmailSection = () => {
-    const[emailSubmitted, setEmailSubmitted]= useState(false)
-    const handleSubmit = async (e) =>{
-        e.preventDefault()
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        }
-        const JSONdata = JSON.stringify(data)
-        const endpoint = "/api/send"
-
-        const options ={
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSONdata
-        }
-
-        const response = await fetch(endpoint, options)
-        const resData = await response.json()
-        console.log(resData)
-        if(response.status === 200){
-            console.log('Message sent.')
-        }
-    }
+    
   return (
     <section className='grid md:grid-row my-12 md:my-12 py-24 gap-4 relative justify-center self-center'>
         
