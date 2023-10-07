0943638bdd03b824d66aa3fabe32610b27e32cb5
HenryLooo <94285262+HenryLooo@users.noreply.github.com>
2023-10-07 09:21:35 +0700

    tried giving {} to image source to fix imgae loader

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -58,7 +58,8 @@ const AboutSection = () => {
   return (
     <section className='text-white'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py=16 xl:px-16'>
-            <Image src="/images/About-US.png" 
+            <Image 
+            src={"/images/About-US.png"} 
             width={500} 
             height={500} 
             alt='aboutusimg'/>
